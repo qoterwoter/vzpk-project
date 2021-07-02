985ba43f3f3f60c2cf819472be9ac9f358e815a8
qoterwoter <marderics@gmail.com>
2021-07-02 18:43:03 +0300

    response modal window

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,4 +1,17 @@
 function showResponse() {
     // console.log('Response')
-    alert("Сообщение отправлено!")
+    background.style.display = 'flex'
+    response.style.display = 'flex'
+}
+let response = document.getElementsByClassName('consultation__response')[0]
+let background = document.getElementsByClassName('respone__background')[0]
+function closeResponse() {
+    background.style.display = 'none'
+    response.style.display = 'none'
+}
+window.onclick = function (event) {
+    if (event.target != response) {
+        background.style.display = 'none'
+        response.style.display = 'none'
+    }
 }
